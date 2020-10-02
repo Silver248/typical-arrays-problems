427a820ca95aa3007d054a0200ac45c0f9ae0dda
Vyacheslav Manzhela <wmanzhela@gmail.com>
2020-10-02 11:58:43 +0300

    feat: finish all parts of the task

--- src/index.js
+++ src/index.js
@@ -14,9 +14,29 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  return 0;
+    let maxi=0;
+    if (!array) {
+        return 0;
+    } else {
+        for (let i=0; i<array.length; i++){
+            if (array[i]>maxi) {
+                maxi=array[i];
+            }
+        }
+        return maxi;
+    }   
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array||array.length==0) {
+        return 0;
+    } else {
+        let aver=0;
+        let sum=0;
+        for (let i=0; i<array.length; i++){
+            sum=sum+array[i];
+        }
+        aver=sum/array.length;
+        return aver;
+    }   
 }
